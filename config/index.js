0979366b94baac563b954fc874c79b19e4be2600
huayijie <114122246+huayijie@users.noreply.github.com>
2022-09-22 11:07:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -62,7 +62,7 @@ export const USER_CONFIG = {
       // 想要发送的人的名字
       name: "吴傻蛋",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "❤",
+      id: "lover",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "ouPAE50R8HSRxJrd6FSRDzAWC3JU",
       // 所在省份
